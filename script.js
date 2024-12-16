a14b034f57e7f2d25fc1f4d72a49abbb6b163984
Maximilian <scientificresearch309@gmail.com>
2024-12-16 12:35:17 +0100

    some more changes in the solution for the challenge

--- script.js
+++ script.js
@@ -22,22 +22,26 @@ const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
 
-  // THE BEST METHOD IS TO USE PROTOTYPE:
-  // accelerate = function () {
+  // THE BEST METHOD IS TO USE PROTOTYPAL INHERITANCE => It would be terrible if we have many many car objects!
+  // this.accelerate = function () {
   //   this.speed += 10;
   // };
 };
 
 // 2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
 
+// PROTOTYPAL INHERITANCE:
 Car.prototype.accelerate = function () {
-  console.log((this.speed += 10));
+  console.log((this.speed += 10)); // this keyword points to the object, on which the method will be called! => Object would be the Car here => BMW and Mercedes!
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 // 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
 
-Car.prototype.break = function () {
+// PROTOTYPAL INHERITANCE:
+Car.prototype.brake = function () {
   console.log((this.speed -= 5));
+  console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
 // 4. Create 2 car objects and expriment with calling 'accelerate' and 'brake' multiple times on each of them.
@@ -45,9 +49,13 @@ Car.prototype.break = function () {
 console.log("-------------BMW------------------");
 const BMW = new Car("BMW", 120);
 BMW.accelerate(); // 130
-BMW.break(); // 125
+BMW.accelerate(); // 140
+BMW.brake(); // 135
+BMW.accelerate(); // 145
 
 console.log("-------------Mercedes-------------");
 const Mercedes = new Car("Mercedes", 95);
 Mercedes.accelerate(); // 105
-Mercedes.break(); // 100
+Mercedes.brake(); // 100
+Mercedes.accelerate(); // 110
+Mercedes.accelerate(); // 120
